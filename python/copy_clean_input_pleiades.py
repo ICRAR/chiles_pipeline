df45be01efef17a3a49fdf04ebd8e7d42d7f876a
KevinVinsen <kvn1961@gmail.com>
2015-06-02 12:40:58 +0800

    More parallelism

--- python/copy_clean_input_pleiades.py
+++ python/copy_clean_input_pleiades.py
@@ -32,7 +32,6 @@ import os
 import shutil
 import sys
 import tarfile
-from pleiades_common import get_expect_combinations
 
 from settings_file import CHILES_BUCKET_NAME
 from s3_helper import S3Helper
@@ -41,24 +40,48 @@ LOG = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)-15s:' + logging.BASIC_FORMAT)
 
 LOG.info('PYTHONPATH = {0}'.format(sys.path))
-#DIRECTORY = '/tmp/kevin/split_vis'
-DIRECTORY = '/mnt/hidata/kevin/split_vis'
+DIRECTORY = '/tmp/kevin/split_vis'
+# DIRECTORY = '/scratch/kevin/split_vis'
 
 
 class KeyData:
-    def __init__(self, key, tail):
+    def __init__(self, key, filename):
         self.key = key
-        self.tail = tail
+        self.filename = filename
 
 
-def copy_files(frequency_id):
+def copy_files(key, key_data):
+    # Queue the copy of the file
+    elements = key.split('/')
+    directory = '{0}/{1}/{2}/'.format(DIRECTORY, elements[1], elements[2])
+    tar_file = os.path.join(directory, 'data.tar.gz' if key_data.filename.endswith('data.tar.gz') else 'data.tar')
+    # noinspection PyBroadException
+    try:
+        LOG.info('key: {0}, tar_file: {1}, directory: {2}, frequency_id: {3}'.format(
+            key_data.key.key,
+            tar_file,
+            directory,
+            elements[1]))
+        if os.path.exists(directory):
+            LOG.info('directory already exists: {0}'.format(directory))
+        else:
+            os.makedirs(directory)
+            key_data.key.get_contents_to_filename(tar_file)
+            with closing(tarfile.open(tar_file, "r:gz" if tar_file.endswith('tar.gz') else "r:")) as tar:
+                tar.extractall(path=directory)
+
+            os.remove(tar_file)
+    except Exception:
+        LOG.exception('Task died')
+        shutil.rmtree(directory, ignore_errors=True)
+
+
+def get_list_data():
     s3_helper = S3Helper()
     bucket = s3_helper.get_bucket(CHILES_BUCKET_NAME)
-    LOG.info('Scanning bucket: {0}, frequency_id: {1}'.format(bucket, frequency_id))
-
+    LOG.info('Scanning bucket: {0}'.format(bucket))
     file_list = {}
-    for key in bucket.list(prefix='CVEL/{0}'.format(frequency_id)):
-        LOG.info('Checking {0}'.format(key.key))
+    for key in bucket.list(prefix='CVEL/'):
         # Ignore the key
         if key.key.endswith('/data.tar.gz') or key.key.endswith('/data.tar'):
             (head, tail) = os.path.split(key.key)
@@ -68,46 +91,41 @@ def copy_files(frequency_id):
             elif tail.endswith('data.tar'):
                 file_list[head] = KeyData(key, tail)
 
-    for head, tail in file_list.iteritems():
-            # Queue the copy of the file
-            elements = head.split('/')
-            directory = '{0}/{1}/{2}/'.format(DIRECTORY, frequency_id, elements[2])
-            tar_file = os.path.join(directory, 'data.tar.gz' if tail.tail.endswith('data.tar.gz') else 'data.tar')
-            # noinspection PyBroadException
-            try:
-                LOG.info('key: {0}, tar_file: {1}, directory: {2}, frequency_id: {3}'.format(
-                    tail.key.key,
-                    tar_file,
-                    directory,
-                    frequency_id))
-                if os.path.exists(directory):
-                    LOG.info('directory already exists: {0}'.format(directory))
-                else:
-                    os.makedirs(directory)
-                    tail.key.get_contents_to_filename(tar_file)
-                    with closing(tarfile.open(tar_file, "r:gz" if tar_file.endswith('tar.gz') else "r:")) as tar:
-                        tar.extractall(path=directory)
+    keys = file_list.keys()
+    keys = sorted(keys)
 
-                    os.remove(tar_file)
-            except Exception:
-                LOG.exception('Task died')
-                shutil.rmtree(directory, ignore_errors=True)
+    return keys, file_list
 
 
 def command_pbs(args):
     # The id is the array id
     array_id = args.array_id
 
-    expected_combinations = get_expect_combinations()
-    copy_files(expected_combinations[array_id])
+    keys, file_list = get_list_data()
+    key = keys[array_id]
+    key_data = file_list.get(key)
+    copy_files(key, key_data)
 
 
-def command_list(args):
+def command_local_test(args):
     # The id is the index
     index = args.index
 
-    expected_combinations = get_expect_combinations()
-    copy_files(expected_combinations[index])
+    keys, file_list = get_list_data()
+    key = keys[index]
+    key_data = file_list.get(key)
+    copy_files(key, key_data)
+
+
+def command_list(args):
+    if args is not None:
+        LOG.error('args should be none')
+    keys, file_list = get_list_data()
+
+    for key in keys:
+        LOG.info('{0}'.format(key))
+
+    LOG.info('Size: {0}'.format(len(keys)))
 
 
 def main():
@@ -119,8 +137,11 @@ def main():
     parser_pbs.set_defaults(func=command_pbs)
 
     parser_galaxy = subparsers.add_parser('list', help='list help')
-    parser_galaxy.add_argument('index', type=int, help='the index for the command line')
     parser_galaxy.set_defaults(func=command_list)
+
+    parser_galaxy = subparsers.add_parser('local_test', help='local test help')
+    parser_galaxy.add_argument('index', type=int, help='the index for the command line')
+    parser_galaxy.set_defaults(func=command_local_test)
 
     args = parser.parse_args()
     args.func(args)
